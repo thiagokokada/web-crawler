93e3f8610fe3d33108688a54daac2a0a9eda582a
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-10-13 21:00:01 -0300

    Add flake.nix file

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "web-crawler";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    {
+      # Nixpkgs overlay providing the application
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: prev: {
+          # The application
+          web-crawler = prev.poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+          };
+        })
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in
+      rec {
+        apps = {
+          web-crawler = pkgs.web-crawler;
+        };
+
+        defaultApp = apps.web-crawler;
+
+        devShell = import ./shell.nix { inherit pkgs; };
+      }));
+}
--- shell.nix
+++ shell.nix
@@ -1,9 +1,7 @@
-{ pkgs ? import (fetchTarball
-  "https://github.com/nixos/nixpkgs/archive/66f4dc4fd1b47e9e06d0f1bd78faffa51f0cc59c.tar.gz") { }, ...
-}:
+{ pkgs ? import <nixpkgs> { } }:
 
 let
-  python-with-packages = pkgs.python38.withPackages (ps: with ps; [ poetry ]);
+  python-with-packages = pkgs.python39.withPackages (ps: with ps; [ poetry ]);
 in pkgs.mkShell {
   buildInputs = with pkgs; [ python-with-packages ];
 }
